9d54fff5f64042a53ba19b51abda7679f3931a5c
Dizzrt <dizzrt.s@qq.com>
2025-10-25 17:30:39 +0800

    add protoc-gen-ellie-go

---
+++ cmd/protoc-gen-ellie-go/go.mod
@@ -0,0 +1,7 @@
+module github.com/dizzrt/ellie/cmd/protoc-gen-ellie-go
+
+go 1.25
+
+require google.golang.org/protobuf v1.36.10
+
+replace google.golang.org/protobuf => github.com/dizzrt/protobuf-go v1.36.10
---
+++ cmd/protoc-gen-ellie-go/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	gengo "google.golang.org/protobuf/cmd/protoc-gen-go/internal_gengo"
+	"google.golang.org/protobuf/compiler/protogen"
+)
+
+const release = "v1.0.0"
+const baseProtobufRelease = "v1.36.10"
+
+var showVersion = flag.Bool("version", false, "print the version and exit")
+
+func main() {
+	flag.Parse()
+	if *showVersion {
+		fmt.Printf("protoc-gen-ellie-go %s\nbased on protobuf-go %s\n", release, baseProtobufRelease)
+		return
+	}
+
+	var (
+		flags        flag.FlagSet
+		importPrefix = flags.String("import_prefix", "", "prefix to prepend to import paths")
+	)
+
+	importRewriteFunc := func(importPath protogen.GoImportPath) protogen.GoImportPath {
+		switch importPath {
+		case "context", "fmt", "math":
+			return importPath
+		}
+
+		if *importPrefix != "" {
+			return protogen.GoImportPath(*importPrefix) + importPath
+		}
+
+		return importPath
+	}
+
+	protogen.Options{
+		ParamFunc:         flags.Set,
+		ImportRewriteFunc: importRewriteFunc,
+	}.Run(func(gen *protogen.Plugin) error {
+		for _, f := range gen.Files {
+			if !f.Generate {
+				continue
+			}
+
+			gengo.GenerateFile(gen, f)
+		}
+
+		gen.SupportedFeatures = gengo.SupportedFeatures
+		return nil
+	})
+}
