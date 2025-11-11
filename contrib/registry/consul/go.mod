ebb7318cff7da40b1b1d619ea9284d50d6c2d5df
Dizzrt <dizzrt.s@qq.com>
2025-11-11 21:33:24 +0800

    update

--- contrib/registry/consul/go.mod
+++ contrib/registry/consul/go.mod
@@ -9,21 +9,29 @@ require (
 
 require (
 	github.com/armon/go-metrics v0.4.1 // indirect
-	github.com/fatih/color v1.16.0 // indirect
+	github.com/dizzrt/filerotator v0.2.1 // indirect
+	github.com/fatih/color v1.18.0 // indirect
 	github.com/go-viper/mapstructure/v2 v2.4.0 // indirect
-	github.com/google/go-cmp v0.7.0 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
-	github.com/hashicorp/go-hclog v1.5.0 // indirect
+	github.com/hashicorp/go-hclog v1.6.3 // indirect
 	github.com/hashicorp/go-immutable-radix v1.3.1 // indirect
+	github.com/hashicorp/go-metrics v0.5.4 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/hashicorp/go-rootcerts v1.0.2 // indirect
-	github.com/hashicorp/golang-lru v0.5.4 // indirect
-	github.com/hashicorp/serf v0.10.1 // indirect
-	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/hashicorp/golang-lru v1.0.2 // indirect
+	github.com/hashicorp/serf v0.10.2 // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/miekg/dns v1.1.68 // indirect
 	github.com/mitchellh/go-homedir v1.1.0 // indirect
-	github.com/stretchr/testify v1.11.1 // indirect
-	golang.org/x/exp v0.0.0-20250808145144-a408d31f581a // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/stretchr/objx v0.5.2 // indirect
+	go.opentelemetry.io/otel v1.38.0 // indirect
+	go.opentelemetry.io/otel/trace v1.38.0 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
+	go.uber.org/zap v1.27.0 // indirect
+	golang.org/x/exp v0.0.0-20250911091902-df9299821621 // indirect
+	golang.org/x/net v0.44.0 // indirect
 	golang.org/x/sys v0.36.0 // indirect
 )
--- contrib/registry/consul/registry.go
+++ contrib/registry/consul/registry.go
@@ -2,7 +2,9 @@ package consul
 
 import (
 	"context"
+	"fmt"
 	"sync"
+	"sync/atomic"
 	"time"
 
 	"github.com/dizzrt/ellie/registry"
@@ -22,25 +24,119 @@ type Registry struct {
 }
 
 func New(apiClient *api.Client, opts ...Option) *Registry {
-	r := &Registry{}
+	r := &Registry{
+		registry:          make(map[string]*serviceSet),
+		enableHealthCheck: true,
+		timeout:           10 * time.Second,
+		cli: &Client{
+			dc:                             SingleDatacenter,
+			cli:                            apiClient,
+			resolver:                       defaultResolver,
+			healthcheckInterval:            10 * time.Second,
+			heartbeat:                      true,
+			deregisterCriticalServiceAfter: 600 * time.Second,
+			cancelers:                      make(map[string]*canceler),
+		},
+	}
+
+	for _, opt := range opts {
+		opt(r)
+	}
 
 	return r
 }
 
 func (r *Registry) Register(ctx context.Context, svc *registry.ServiceInstance) error {
-	return nil
+	return r.cli.Register(ctx, svc, r.enableHealthCheck)
 }
 
 func (r *Registry) Deregister(ctx context.Context, svc *registry.ServiceInstance) error {
-	return nil
+	return r.cli.Deregister(ctx, svc.ID)
 }
 
 func (r *Registry) GetService(ctx context.Context, serviceName string) ([]*registry.ServiceInstance, error) {
-	return nil, nil
+	r.lock.RLock()
+	set := r.registry[serviceName]
+	r.lock.RUnlock()
+
+	getRemote := func() []*registry.ServiceInstance {
+		services, _, err := r.cli.Service(ctx, serviceName, 0, true)
+		if err == nil && len(services) > 0 {
+			return services
+		}
+
+		return nil
+	}
+
+	if set == nil {
+		if s := getRemote(); len(s) > 0 {
+			return s, nil
+		}
+
+		return nil, fmt.Errorf("service %s not resolved in registry", serviceName)
+	}
+
+	ss, _ := set.services.Load().([]*registry.ServiceInstance)
+	if ss == nil {
+		if s := getRemote(); len(s) > 0 {
+			return s, nil
+		}
+
+		return nil, fmt.Errorf("service %s not found in registry", serviceName)
+	}
+
+	return ss, nil
 }
 
 func (r *Registry) Watch(ctx context.Context, serviceName string) (registry.Watcher, error) {
-	return nil, nil
+	if err := ctx.Err(); err != nil {
+		return nil, err
+	}
+
+	r.lock.Lock()
+	set, ok := r.registry[serviceName]
+	if !ok {
+		cancelCtx, cancel := context.WithCancel(context.Background())
+		set = &serviceSet{
+			registry:    r,
+			watcher:     make(map[*watcher]struct{}),
+			services:    &atomic.Value{},
+			serviceName: serviceName,
+			ctx:         cancelCtx,
+			cancel:      cancel,
+		}
+
+		r.registry[serviceName] = set
+	}
+
+	set.ref.Add(1)
+	r.lock.Unlock()
+
+	w := &watcher{
+		event: make(chan struct{}, 1),
+	}
+
+	w.ctx, w.cancel = context.WithCancel(ctx)
+	w.set = set
+	set.lock.Lock()
+	set.watcher[w] = struct{}{}
+	set.lock.Unlock()
+
+	ss, _ := set.services.Load().([]*registry.ServiceInstance)
+	if len(ss) > 0 {
+		select {
+		case w.event <- struct{}{}:
+		default:
+		}
+	}
+
+	if !ok {
+		if err := r.resolve(ctx, set); err != nil {
+			return nil, err
+		}
+	}
+
+	return w, nil
 }
 
 func (r *Registry) tryDelete(ss *serviceSet) bool {
@@ -49,7 +145,79 @@ func (r *Registry) tryDelete(ss *serviceSet) bool {
 	if ss.ref.Add(-1) != 0 {
 		return false
 	}
+
 	ss.cancel()
 	delete(r.registry, ss.serviceName)
 	return true
+}
+
+func (r *Registry) resolve(ctx context.Context, ss *serviceSet) error {
+	listServices := r.cli.Service
+	if r.timeout > 0 {
+		listServices = func(ctx context.Context, service string, index uint64, passingOnly bool) ([]*registry.ServiceInstance, uint64, error) {
+			timeoutCtx, cancel := context.WithTimeout(ctx, r.timeout)
+			defer cancel()
+
+			return r.cli.Service(timeoutCtx, service, index, passingOnly)
+		}
+	}
+
+	services, idx, err := listServices(ctx, ss.serviceName, 0, true)
+	if err != nil {
+		return err
+	}
+
+	if len(services) > 0 {
+		ss.broadcast(services)
+	}
+
+	go func() {
+		ticker := time.NewTicker(time.Second)
+		defer ticker.Stop()
+
+		for {
+			select {
+			case <-ticker.C:
+				tempService, tempIndex, err := listServices(ss.ctx, ss.serviceName, idx, true)
+				if err != nil {
+					if err := sleepCtx(ss.ctx, time.Second); err != nil {
+						return
+					}
+
+					continue
+				}
+
+				if len(tempService) != 0 && tempIndex != idx {
+					services = tempService
+					ss.broadcast(services)
+				}
+
+				idx = tempIndex
+
+			case <-ss.ctx.Done():
+				return
+			}
+		}
+	}()
+
+	return nil
+}
+
+func (r *Registry) ListServices() (allServices map[string][]*registry.ServiceInstance, err error) {
+	r.lock.RLock()
+	defer r.lock.RUnlock()
+
+	allServices = make(map[string][]*registry.ServiceInstance)
+	for name, set := range r.registry {
+		var services []*registry.ServiceInstance
+		ss, _ := set.services.Load().([]*registry.ServiceInstance)
+		if ss == nil {
+			continue
+		}
+
+		services = append(services, ss...)
+		allServices[name] = services
+	}
+
+	return
 }
